fdb98b765f93e36fff13bfc3038d5e31419f955f
Josh Klimaszewski <joshklimaszewski@Joshs-MacBook-Pro.local>
2018-03-23 21:40:15 -0400

    starting videos-list

--- src/components/lists/videos-list.js
+++ src/components/lists/videos-list.js
@@ -4,7 +4,10 @@ const VideosList = () => {
     
     return (
         <div className="videos-list">
-        videos list
+            <div className="video-thumb video-one"></div>
+            <div className="video-thumb video-two"></div>
+            <div className="video-thumb video-three"></div>
+            <div className="video-thumb video-four"></div>
         </div>
     );
 }
